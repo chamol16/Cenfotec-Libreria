1b47b2e2e8e0ecfb58d5c3840c5985f3882c1642
Victor V <chamol1602@gmail.com>
2022-04-10 02:18:11 -0600

    payment method added

--- ProjectGM_JS/perfil.js
+++ ProjectGM_JS/perfil.js
@@ -8,3 +8,26 @@ if (userConnected) {
 logOut = () => {
   localStorage.clear();
 };
+
+inicioElementoA.addEventListener("click", logOut);
+
+/*INFORMACION DE USUARIO*/
+document.getElementById("nombreUsuario").textContent = userConnected.nombre;
+document.getElementById("input-first-last-name").textContent =
+  userConnected.primerApellido;
+document.getElementById("input-second-last-name").textContent =
+  userConnected.segundoApellido;
+document.getElementById("slt-gender").textContent = userConnected.genero;
+document.getElementById("slt-type-id").textContent = userConnected.tipoId;
+document.getElementById("input-id").textContent = userConnected.id;
+document.getElementById("input-email").textContent = userConnected.correo;
+document.getElementById("input-password").textContent =
+  userConnected.contrasena;
+document.getElementById("slt-province").textContent = userConnected.provincia;
+document.getElementById("slt-canton").textContent = userConnected.canton;
+document.getElementById("slt-district").textContent = userConnected.distrito;
+document.getElementById("input-address").textContent = userConnected.direccion;
+document.getElementById("generos-favoritos").textContent =
+  userConnected.generoFavorito;
+document.getElementById("autores-favoritos").textContent =
+  userConnected.autorFavorito;
